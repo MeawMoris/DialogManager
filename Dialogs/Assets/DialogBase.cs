a8d6a0a2ac563851a5a537d574eae59151284d24
Moris rafol <moris.rafol@gmail.com>
2018-01-23 03:46:13 +0200

    starting point
    -Added textmesh pro asset to unity project

---
+++ Dialogs/Assets/DialogBase.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogBase : MonoBehaviour {
+
+   /*
+    * Dialog types:
+    * -monologue
+    * -dialog between more than one person (probably 2)
+    * 
+    * 
+    * -interaction with items dialog
+    * -world items dialog(w/o interaction)
+    * 
+    * 
+    * to do steps:
+    * 1)make a WinForm that creates manages the dialog/monologue and exports to an xml
+    * 2)create a library/classes that read the xml file and turn it into an object instance
+    * 3)display the dialog/monologue in unity
+    */
+    
+
+}
